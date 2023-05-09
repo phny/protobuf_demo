3ef2559b05c4cf7d6a9eb83c498256edab28d73d
heyulin <1063766703@qq.com>
2023-05-09 23:33:31 +0800

    fix: file not close bug

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,14 @@ int main(int argc, char* argv) {
     std::string buffer;
     message.SerializeToString(&buffer);
 
+    std::cout << message.id() << std::endl;
+    std::cout << message.str() << std::endl;
+    std::cout << message.opt() << std::endl;
+
     // 将序列化后的字节数组写入文件
     std::ofstream output("message.bin", std::ios::binary);
     output.write(buffer.data(), buffer.size());
+    output.close(); // 这里的close不能少，否则下面的message2的值会显示不出来
 
     // 从文件中读取序列化后的字节数组
     std::ifstream input("message.bin", std::ios::binary);
@@ -24,6 +29,7 @@ int main(int argc, char* argv) {
     // 反序列化消息
     helloworld::helloworld message2;
     message2.ParseFromString(input_buffer);
+    input.close();
 
     // 输出消息内容
     std::cout << "ID: " << message2.id() << std::endl;
